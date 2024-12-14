fdf8edb695a97a4c1b7492b224af733747127cc1
Annie Ehler <annie.ehler.4@gmail.com>
2024-12-13 16:18:31 -0800

    Configure the repository site

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: "Annie's HomeLab",
+    description: 'My personal repository of Kasm supported workspaces.',
+    icon: 'https://annie444.github.io/workspaces/1.0/image.png',
+    listUrl: 'https://annie444.githib.io/workspaces/',
+    contactUrl: 'https://github.com/annie444/workspaces/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/workspaces/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
